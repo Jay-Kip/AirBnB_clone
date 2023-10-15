5b3b471c351db7310a95955fd7414fd2051383af
HamO001 <ndarugastiv9@gmail.com>
2023-10-15 14:21:09 -0700

    store

---
+++ storage.py
@@ -0,0 +1,95 @@
+dfkvzdm
+fvof
+ffpfld
+fd'fd
+ff;
+fdld;f
+df;lf
+;v
+d;z
+
+;]g
+z;z;
+\;\
+
+        \;
+        \g\poklvvm,.
+        vllflz;\'"
+
+        ,\vldl;;dfld;d
+        dldld,d
+        ddkkd
+        dd
+        ddkdkd
+        dkkd
+        djkd
+        dkjd
+        d
+
+
+        d
+        f
+        f
+        d
+
+        df
+
+        \
+                f
+                f
+                f
+
+                f
+                z
+                f
+                fd
+                fz
+
+                v
+                g
+                ff
+
+                f
+
+
+
+
+                f
+                f
+                f
+                f
+                f
+                f
+                f
+
+                f
+                f
+            f
+            f
+            f
+            f
+            fz
+            f
+        z
+        f
+        z
+        
+        z
+        f
+        f   ;dfff
+        f
+        f
+
+
+        g
+        g
+        g
+        g
+        g
+        f
+
+
+        z
+        df
+        z
+        f
