fac6fd84f0b14629a0b7c3eb37ca93d698a43db2
HamO001 <ndarugastiv9@gmail.com>
2023-10-15 13:07:41 -0700

    weh

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,23 @@ from datetime import datetime
 
 class BaseModel:
     '''Defining the base model'''
-
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a BaseModel instance
+        """
+        from models import storage
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+"""
     def __init__(self, *args, **kwargs):
         '''Initializes a new instance'''
         from models import storage
@@ -33,11 +49,13 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            """
 
     def __str__(self):
         '''
         Returns string representation of an instance
         '''
+        class_name = type(self).__name__
         return "[{}] ({}) {}".format(
                 self.__class__.__name__,
                 self.id,
@@ -53,6 +71,19 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
+        """
+         returns dictionary containing keys/values of __dict__ of instance
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+            if key in ("created_at", "updated_at"):
+                value = self.__dict__[key].isoformat()
+                dictionary[key] = value
+        return dictionary
+"""
+    def to_dict(self):
         '''
         Saves to a dictionary
         '''
@@ -61,3 +92,4 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+        """
