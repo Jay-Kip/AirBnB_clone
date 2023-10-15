f00f08c5b309b33ac39d0d755f9d2cdfabbf1781
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-15 05:12:37 +0300

    Air-bnb clone the console

--- console.py
+++ console.py
@@ -16,14 +16,48 @@ CLASSES = [
         ]
 
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
+def check_args(args):
+    '''
+    checks args
+    '''
+    arg_list = parse(args)
+
+    if len(arg_list) == 0:
+        print("** class name missing **")
+    elif arg_list[0] not in CLASSES:
+        print("** class doesn't exist **")
+    else:
+        return arg_list
+
+
 def all(self):
     '''
     Returns dict objs
     '''
     return self.__objects
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    storage = models.storage
 
     def do_quit(self, arg):
         """
@@ -67,7 +101,11 @@ class HBNBCommand(cmd.Cmd):
         '''
         Create a new instance of basemodel
         '''
-        args = line.split()
+        args = check_args(line)
+        if args:
+            print(eval(args[0])().id)
+            self.storage.save()
+        '''args = line.split()
         if not args:
             print("** class name missing **")
             return
@@ -76,8 +114,8 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         new_obj = CLASSES
-        new_obj.save()
-        print(new_obj.id)
+        self.storage.save()
+        print(new_obj.id)'''
 
     def do_show(self, line):
         '''
@@ -124,52 +162,52 @@ class HBNBCommand(cmd.Cmd):
         models.storage.all().pop(key)
         models.storage.save()
 
-        def do_all(self, line):
-            '''
-            print all string representation of instances
-            '''
-            arg_list = line.split()
-            objects = self.storage.all().values()
-            if not arg_list:
-                print([str(obj) for obj in objects])
-            else:
-                if arg_list[0] not in CLASSES:
-                    print("** class doesn't exist **")
-                else:
-                    print([str(obj) for obj in objects
-                           if arg_list[0] in str(obj)])
-
-        def do_update(self, line):
-            '''
-            Updates an instance based on the class name and id
-            '''
-            args = line.split()
-            if not args:
-                print("** class name missing **")
-                return
-            class_name = args[0]
-            if class_name not in CLASSES:
+    def do_all(self, line):
+        '''
+        print all string representation of instances
+        '''
+        arg_list = line.split()
+        objects = self.storage.all().values()
+        if not arg_list:
+            print([str(obj) for obj in objects])
+        else:
+            if arg_list[0] not in CLASSES:
                 print("** class doesn't exist **")
-                return
-            if len(args) < 2:
-                print("** instance id missing **")
-                return
-            obj_id = args[1]
-            key = class_name + "." + obj_id
-            if key not in models.storage.all():
-                print("** no instance found **")
-                return
-            if len(args) < 3:
-                print("** attribute name missing **")
-                return
-            if len(args) < 4:
-                print("** value missing **")
-                return
-            attribute_name = args[2]
-            attribute_value = args[3]
-            obj = models.storage.all()[key]
-            setattr(obj, attribute_name, attribute_value)
-            obj.save()
+            else:
+                print([str(obj) for obj in objects
+                       if arg_list[0] in str(obj)])
+
+    def do_update(self, line):
+        '''
+        Updates an instance based on the class name and id
+        '''
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = class_name + "." + obj_id
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attribute_name = args[2]
+        attribute_value = args[3]
+        obj = models.storage.all()[key]
+        setattr(obj, attribute_name, attribute_value)
+        obj.save()
 
 
 if __name__ == '__main__':
