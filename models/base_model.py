3c0312b1081535bc7c7e50504da553405b8e5d86
HamO001 <ndarugastiv9@gmail.com>
2023-10-15 13:48:50 -0700

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-'''
-Base model that defines all common attributes/methods for other classes
-'''
+"""
+Module that implements the base model class
+"""
 
-from uuid import uuid4
 from datetime import datetime
-# from models.engine import file_storage
-# from models import storage
+from uuid import uuid4
 
 
 class BaseModel:
-    '''Defining the base model'''
+    """
+    A class that defines common attr/methods for other classes
+    """
+
     def __init__(self, *args, **kwargs):
         """
         Initializes a BaseModel instance
@@ -27,47 +28,20 @@ class BaseModel:
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
-"""
-    def __init__(self, *args, **kwargs):
-        '''Initializes a new instance'''
-        from models import storage
-        if kwargs:
-            '''storage.new(self)'''
-
-            for key, value in kwargs.items():
-                if key != "__class__":
-
-                    if key in ("created_at", "updated_at"):
-                        '''convert datetime string to detetime object
-                        '''
-                        setattr(self, key, datetime.fromisoformat(value))
-
-                    else:
-                        setattr(self, key, value)
-
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            """
 
     def __str__(self):
-        '''
-        Returns string representation of an instance
-        '''
+        """
+        Prints [<class name>] (<self.id>) <self.__dict__>
+        """
         class_name = type(self).__name__
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__,
-                self.id,
-                self.__dict__
-                )
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        '''
-        Saves the instance
-        '''
+        """
+        updates public instance attribute updated_at with current datetime
+        """
         from models import storage
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
@@ -82,14 +56,3 @@ class BaseModel:
                 value = self.__dict__[key].isoformat()
                 dictionary[key] = value
         return dictionary
-"""
-    def to_dict(self):
-        '''
-        Saves to a dictionary
-        '''
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-        """
