b5513c7400382e8a51a83bceccd6b33a713dd3cc
HamO001 <ndarugastiv9@gmail.com>
2023-10-15 12:49:02 -0700

    air

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,7 +57,7 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path) as file:
                 obj_dict = json.load(file)
-                for o in objdict.values():
+                for o in obj_dict.values():
                     class_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(class_name)(**o))
